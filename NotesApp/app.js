adcc0f08f12edce4c42fa3afc602fcbd4d211f5c
Hamid <59122894+DarkAmdHa@users.noreply.github.com>
2021-08-21 23:56:58 +0500

    Update app.js

--- NotesApp/app.js
+++ NotesApp/app.js
@@ -27,13 +27,19 @@ yargs.command({
 
 //Remove Command
 yargs.command({
-   command: 'remove',
-   describe: 'Remove a note',
-   handler: function(){
-       console.log('Removing Note!')
-   } 
+    command: 'remove',
+    describe: 'Remove a note',
+    builder: {
+        title: {
+            describe: 'Title of the note to be removed',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv){
+        notes.removeNote(argv.title);
+    }
 });
-
 yargs.command({
     command: 'list',
     describe: 'List all notes',
